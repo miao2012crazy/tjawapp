309fe49964cd8ce44ce2ecd63702364c891f95ae
miao2012crazy <miao2012crazy@163.com>
2019-05-06 16:11:13 +0800

    Initial commit

---
+++ app/src/main/java/com/zysoft/tjawshapingapp/applaction/CustomApplaction.java
@@ -0,0 +1,15 @@
+package com.zysoft.tjawshapingapp.applaction;
+
+import com.zysoft.baseapp.BaseApplaction.BaseApplaction;
+
+/**
+ * Created by mr.miao on 2019/5/6.
+ */
+
+public class CustomApplaction extends BaseApplaction{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/zysoft/tjawshapingapp/constants/AppConstant.java
@@ -0,0 +1,11 @@
+package com.zysoft.tjawshapingapp.constants;
+
+/**
+ * Created by mr.miao on 2019/5/6.
+ */
+
+public class AppConstant {
+
+
+
+}
